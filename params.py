153000e7c275d9661da685ed8b7faf0e9cf4b5ba
antoine lepareux <a.lepareux@gmail.com>
2025-12-02 16:33:11 +0100

    preprocessor update with chunk threshold + params.py

---
+++ params.py
@@ -0,0 +1,5 @@
+#Size of the chunk to train the model (e.g. 256 x 256)
+CHUNK_SIZE = 256
+
+#Threshold of class null > if 0.1 -> chunk with less than 10% of class null is cleared for model training
+THRESHOLD_0 = 0.1
