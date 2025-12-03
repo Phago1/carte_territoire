1a80ab82ca7030f1e9f94c672f14cb1e657bfd92
Jean Montenegro <j.esteban.montenegro@gmail.com>
2025-12-03 17:55:27 +0100

    end of day, registry under construction. a bit dirty and mix with taxifare

--- dl_logic/registry.py
+++ dl_logic/registry.py
@@ -1,2 +1,95 @@
 # Here we save the results and the model
 # We also add a load function
+
+# libraries
+from params import *
+
+import pickle
+import os
+import time
+
+from tensorflow import keras
+from google.cloud import storage
+
+import mlflow
+from mlflow.tracking import MlflowClient
+
+# functions
+# metric Iou
+
+"""
+dans le main il faut créer un dictionnaire pour params et metrics aves les valeurs qu'on veut sauvegarder
+metrics c'est Iou
+dans le main:
+Iou = dict(Iou = np.min(history.history['iou']))
+params = dict(
+        context="train",
+        class_number= ...,
+        number_of_chunks=len(...),
+        reduction_mask = True
+)
+"""
+def save_results(params: dict, metrics: dict):
+
+    if MODEL_TARGET == "mlflow":
+        if params is not None:
+            mlflow.log_params(params)
+        if metrics is not None:
+            mlflow.log_metrics(metrics)
+        print("✅ Results saved on MLflow")
+
+
+    # Save params locally
+    if params is not None:
+        params_path = os.path.join(LOCAL_REGISTRY_PATH, "params", timestamp + ".pickle")
+        with open(params_path, "wb") as file:
+            pickle.dump(params, file)
+
+    # Save metrics locally
+    if metrics is not None:
+        metrics_path = os.path.join(LOCAL_REGISTRY_PATH, "metrics", timestamp + ".pickle")
+        with open(metrics_path, "wb") as file:
+            pickle.dump(metrics, file)
+
+    print("✅ Results saved locally")
+
+def save_model(model: keras.Model = None) -> None:
+    """
+    Persist trained model locally on the hard drive at f"{LOCAL_REGISTRY_PATH}/models/{timestamp}.h5"
+    - if MODEL_TARGET='gcs', also persist it in your bucket on GCS at "models/{timestamp}.h5" --> unit 02 only
+    - if MODEL_TARGET='mlflow', also persist it on MLflow instead of GCS (for unit 0703 only) --> unit 03 only
+    """
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+
+    # Save model locally
+    model_path = os.path.join(LOCAL_REGISTRY_PATH, "models", f"{timestamp}.h5")
+    model.save(model_path)
+
+    print("✅ Model saved locally")
+
+    if MODEL_TARGET == "gcs":
+        # 🎁 We give you this piece of code as a gift. Please read it carefully! Add a breakpoint if needed!
+
+        model_filename = model_path.split("/")[-1] # e.g. "20230208-161047.h5" for instance
+        client = storage.Client()
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(f"models/{model_filename}")
+        blob.upload_from_filename(model_path)
+
+        print("✅ Model saved to GCS")
+
+        return None
+
+    if MODEL_TARGET == "mlflow":
+        mlflow.tensorflow.log_model(
+            model=model,
+            artifact_path="model",
+            registered_model_name=MLFLOW_MODEL_NAME
+        )
+
+        print("✅ Model saved to MLflow")
+
+        return None
+
+    return None
--- params.py
+++ params.py
@@ -6,6 +6,15 @@ CHUNK_SIZE = 256
 #Threshold of class null > if 0.1 -> chunk with less than 10% of class null is cleared for model training
 THRESHOLD_0 = 0.1
 
-# DATA_LOCATION dans .env est soit gcs pour google cloud storage
-#                             soit local
+# DATA_LOCATION dans .env est - gcs pour google cloud storage
+#                             - local
 DATA_LOCATION = os.environ.get('DATA_LOCATION')
+
+# MODEL_TARGET dans .env est - mlflow
+#                            - gcs
+#                            - local
+MODEL_TARGET = os.environ.get('MODEL_TARGET')
+BUCKET_NAME  = os.environ.get('BUCKET_NAME')
+
+# Where we
+LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'), "Code", "Phago1","training_outputs")
