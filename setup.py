965ccd876e8d57e5d720d843b7ba144fb30678ca
antoine lepareux <a.lepareux@gmail.com>
2025-12-03 09:43:13 +0100

    ajout de setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='carte_territoire',
+      version="0.1",
+      description="Projet carte et territoire",
+      install_requires=requirements,
+      packages=find_packages())
