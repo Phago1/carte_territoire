6af0dd7a7086900304b612d4aaf8f72e7c8fbef9
antoine lepareux <a.lepareux@gmail.com>
2025-12-03 17:16:46 +0100

    ebauche de main.py + rajout du paramètre label_reduction

--- params.py
+++ params.py
@@ -1,5 +1,8 @@
-#Size of the chunk to train the model (e.g. 256 x 256)
+# Size of the chunk to train the model (e.g. 256 x 256)
 CHUNK_SIZE = 256
 
-#Threshold of class null > if 0.1 -> chunk with less than 10% of class null is cleared for model training
+# Threshold of class null > if 0.1 -> chunk with less than 10% of class null is cleared for model training
 THRESHOLD_0 = 0.1
+
+# Reduction of number of classes from 15 to 7 : True or False, if True we work in 7 classes
+LBL_REDUCTION = False
