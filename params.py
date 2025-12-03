2318d14572f632a47bf20f46b7fa87dda8062a01
Jean Montenegro <j.esteban.montenegro@gmail.com>
2025-12-03 16:37:32 +0100

    variable environnement pour le choixde la data, d ou on charge

--- params.py
+++ params.py
@@ -1,5 +1,11 @@
+import os
+
 #Size of the chunk to train the model (e.g. 256 x 256)
 CHUNK_SIZE = 256
 
 #Threshold of class null > if 0.1 -> chunk with less than 10% of class null is cleared for model training
 THRESHOLD_0 = 0.1
+
+# DATA_LOCATION dans .env est soit gcs pour google cloud storage
+#                             soit local
+DATA_LOCATION = os.environ.get('DATA_LOCATION')
